021f6436c099ac4b8b605b901251109aa1a139e7
Katherine Huang <kat.m.huang@gmail.com>
2018-03-24 15:52:09 -0400

    got for loop working

--- js/script2.js
+++ js/script2.js
@@ -7,19 +7,11 @@ var colors = {};
 	colors['yellow'] = '#fc0';
 
 // layers
-var layers = {};
-
 var mercuryLayers = [ '5,darkblue', '5,lightblue', '1,yellow', '2,orange' ];
-layers['Mercury'] = mercuryLayers;
-
 var venusLayers = [ '5,darkblue', '5,lightblue', '1,yellow', '2,orange' ];
-layers['Venus'] = venusLayers;
-
-var earthLayers = [ '2,red', '3,redorange', '5,orange', '3,yelloworange', '2,yellow' ];
-layers['Earth'] = earthLayers;
-
+//var earthLayers = [ '2,red', '3,redorange', '5,orange', '3,yelloworange', '2,yellow' ];
+var earthLayers = [ '2,red', '2,yellow' ];
 var marsLayers = [ '5,darkblue', '5,lightblue', '1,yellow', '2,orange' ];
-layers['Mars'] = marsLayers;
 
 // set up render
 var scene = new THREE.Scene();
@@ -31,33 +23,24 @@ renderer.setSize( canvas.offsetWidth, canvas.offsetHeight );
 
 document.querySelector('section').appendChild(renderer.domElement);
 
-// create objects
-
 // group of everything
 var group = new THREE.Group();
 
-// groups for individual layers (need to be group bc geom + texture)
-var layer1 = new THREE.Group(); // 1 is outside, higher # is deeper within
-var layer2 = new THREE.Group();
-
+// keep the same loader and img
 var loader = new THREE.TextureLoader();
-var imgURL = 'https://eoimages.gsfc.nasa.gov/images/imagerecords/57000/57735/land_ocean_ice_cloud_2048.jpg'
-loader.load(imgURL, function(texture){
-	sphere1 = new THREE.SphereGeometry(2, 35, 35); // radius, segments, rings
-	sphere2 = new THREE.SphereGeometry(1.5, 25, 25); // radius, segments, rings
+var imgURL = 'https://eoimages.gsfc.nasa.gov/images/imagerecords/57000/57735/land_ocean_ice_cloud_2048.jpg';
 
-	material1 = new THREE.MeshBasicMaterial({map: texture, overdraw: 0.5});
-	material1.needsUpdate = true; // not sure if necessary
-	material2 = new THREE.MeshBasicMaterial({color: 0x00ff00});
-
-	mesh1 = new THREE.Mesh(sphere1, material1);
-	mesh2 = new THREE.Mesh(sphere2, material2);
-
-	layer1.add(mesh1);
-	layer2.add(mesh2);
-});
-
-group.add(layer1, layer2);
+// build layers
+for (var i = 0; i < earthLayers.length; i++) {
+	loader.load(imgURL, function(texture){
+		layer = new THREE.Group();
+		sphere = new THREE.SphereGeometry(i, 30, 30);
+		material = new THREE.MeshBasicMaterial({map: texture, overdraw: 0.5});
+		mesh = new THREE.Mesh(sphere, material);
+		layer.add(mesh);
+		group.add(mesh);
+	});
+}
 scene.add(group);
 
 camera.position.z = 5;
